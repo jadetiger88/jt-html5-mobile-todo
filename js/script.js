8591f48bc24c467453859416899f91fd00d126ea
JadeTiger88 <jadetiger88@gmail.com>
2015-09-07 10:58:34 -0700

    Add comments

--- js/script.js
+++ js/script.js
@@ -2,62 +2,84 @@ var list = [];
 var currentObj = {}; 
 
 
+// Refresh the todo list everytime we display #home
 $(document).on('pageshow', '#home', function() {
 	refreshTodoList();
 }); 
 
 $(document).ready(function(){
 
-	// refreshTodoList(); 
-
+    // Process addForm submit
 	$("#addForm").submit(function() {
 
 	    Obj = {"name": $("#addName").val(), "date": $("#addDate").val()}; 
-		if (addTodo(Obj)) { 
-			$("#addName").val("");
-			$("#addDate").val("");
-		}
-		return false; 
-	})
 
+        /* 
+        ** Clear the add fields if added sucessfully. if not leave the fields
+        ** along so the user don't have to re-enter everthing when s/he comes 
+        ** back to this form.
+        */ 
+		if (addTodo(Obj)) { 
+			clearAddFields();
+		}
+
+		return false; 
+	});
+
+     // Process editform submit
 	$("#editForm").submit(function() {
 
 	    newObj = { "name": $("#editName").val(), 
 				   "date": $("#editDate").val()}; 
 
+        // Only do the update if there are changes else do nothing
 		if (JSON.stringify(newObj) != JSON.stringify(currentObj)) {
 			deleteTodo(currentObj);
 			addTodo(newObj); 
 		}
 
 		return false;
-	})
+	});
 
+    // Process add cancel button
 	$("#addCancelButton").click(function() {
-		$("#addName").val("");
-		$("#addDate").val("");
-	})
 
+		// Clears the field if the user abort's the add operation.
+		clearAddFields();
+	});
+
+    // Process the clear operation 
 	$("#clearButton").click(function() {
+
+		// Remove the entire to do list.
 		localStorage.clear(); 
 		list = [];
 		refreshTodoList();
+
 	});
 
+    // Process the single entry todo item click
     // $('.todoLinks').click(function() {
 	$('#todos').on('click','.todoLinks',function(){
+
+		/* 
+		** Get the value of the item clicked and 
+		** put the info on the edit form.
+		*/
      	$("input[name=editName]").val($(this).data("todoname")); 
     	$("input[name=editDate]").val($(this).data("tododate")); 
 	    currentObj = { "name":  $("input[name=editName]").val(), 
 				 	    "date" : $("input[name=editDate]").val()}; 
     });
 
+    // Process the single entry delete operation
     $('#delete').click(function() {
     	deleteTodo(currentObj); 
-    })
+    });
 
-})
+});
 
+// Reresh the to do list. 
 function refreshTodoList() {
 	var i = 0; 
 
@@ -77,6 +99,7 @@ function refreshTodoList() {
 	}
 }
 
+// Add an item to the localstorage list  and the in memory list
 function addTodo(obj) {
 		if (obj.name == "") {
 			alert("Please enter a title for the todo")
@@ -98,6 +121,7 @@ function addTodo(obj) {
 		return true; 
 }
 
+// Remove an item to the localstorage list  and the in memory list
 function deleteTodo(obj) {
 
 	var objStr = JSON.stringify(obj); 
@@ -111,5 +135,11 @@ function deleteTodo(obj) {
 	}
 	localStorage.setItem("todoList", JSON.stringify(list));
 
+}
+
+// Clear the add fields
+function clearAddFields() {
+	$("#addName").val("");
+	$("#addDate").val("");
 }
 
